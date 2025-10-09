c14fae96a2bc1ca64e9400396fea21ec33272b79
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-10-08 21:05:43 -0300

    integração com o bando postgresql (settings e admin)

--- portal/settings.py
+++ portal/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LocalNews',
+        'USER': 'postgres',
+        'PASSWORD': 'senai103',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
