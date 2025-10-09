6f61e85820d1ecd58d0466028b3e5b592f7b7949
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-10-09 20:05:34 -0300

    models criados

--- noticias/models.py
+++ noticias/models.py
@@ -1,25 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Noticia(models.Model):
-    CATEGORIAS = [
-        ('política', 'Política'),
-        ('esportes','Esportes'),
-        ('entretenimento','Entretenimento'),
-        ('tecnologia','Tecnologia'),
-        ('saúde', 'Saúde'),
-        ('economia','Economia'),
-        ('outros','Outros')
-    ]
-    image = models.ImageField("Imagem", upload_to='receitas/images/', blank=True, null=True)
+    image = models.ImageField("Imagem", upload_to='noticias/images/', blank=True, null=True)
     
     titulo=models.CharField("Título", max_length=200)
-    categoria = models.CharField("Categoria", max_length=50, choices=CATEGORIAS, default='Outros')
-    
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, related_name="noticias", verbose_name="Categoria")
+    # categoria = models.CharField("Categoria", max_length=50, choices=CATEGORIAS, default='Outros')
+    usuario = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='Noticias',
+        verbose_name='Usuário',
+        null=True,
+    ) 
+    bairro = models.ForeignKey('Bairro', on_delete=models.CASCADE, related_name='noticias', verbose_name='Bairro', default=1)
     descricao=models.CharField("Descrição", max_length=255)
     introducao=models.TextField("Introdução")
     desenvolvimento_inicial=models.TextField("Desenvolvimento")
-    video = models.TextField("Vídeo", help_text='link do youtube')
+    video = models.TextField("Vídeo", help_text='link do youtube', blank=True)
     desenvolvimento_final=models.TextField("Desenvolvimento")
     conclusao=models.TextField("Conclusão")
     
@@ -42,3 +42,49 @@ class Noticia(models.Model):
         verbose_name = "Notícia"
         verbose_name_plural = "Notícias"
         ordering = ['-created_at'] #ordena as receitas pela data de criação (mais novas primeiro)
+
+
+class Comentario(models.Model):
+    usuario = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comentarios',
+        verbose_name='Usuário',
+    )
+    
+    noticia = models.ForeignKey(
+        Noticia,
+        on_delete=models.CASCADE,
+        related_name='comentarios',
+        verbose_name='Notícia',
+    )
+    
+    comentario = models.TextField("Comentário")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self): 
+        return self.comentario  # Limita no print
+
+    class Meta:
+        verbose_name = "Comentario"
+        verbose_name_plural = "Comentarios"
+        ordering = ['-created_at']
+
+class Categoria(models.Model):
+    categoria = models.CharField("Categoria", max_length=50)
+    
+    def __str__(self): 
+        return self.categoria
+
+class Bairro(models.Model):
+    bairro = models.CharField("Bairro", max_length=150)
+    
+    def __str__(self): 
+        return self.bairro
+    
+class Perfil(models.Model):
+    perfil = models.CharField("Perfil", max_length=50)
+    
+    def __str__(self):
+        return self.perfil
+    
