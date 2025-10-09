ed5958ae5bbac7916b320935b1812392f00b91c0
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-10-09 20:06:19 -0300

    admin att

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Noticia, Categoria, Bairro
+from .models import Noticia, Categoria, Bairro, Comentario
 
 # Register your models here.
 admin.site.register(Noticia)
 admin.site.register(Categoria)
 admin.site.register(Bairro)
+admin.site.register(Comentario)
