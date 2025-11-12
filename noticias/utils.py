7f05d26cf7b4e19e8be9ce2593274ed656c7f5f1
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-11-12 20:52:47 -0300

    ia funcionando

--- noticias/utils.py
+++ noticias/utils.py
@@ -26,7 +26,7 @@ def analisar_texto_noticia(texto):
         
     prob_real = probs[0][0].item()
     print(f"Prob_REAL: {prob_real}")
-    if prob_real < 0.7:
+    if prob_real < 0.95:
         label = 1  # FAKE
     else:
         label = 0  # REAL
