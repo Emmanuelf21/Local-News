76e208187374676b599fa8e5773bd3163ae586a2
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2025-11-23 15:53:59 -0300

    GET, POST, PUT e DELETE completos

--- noticias/api/serializers.py
+++ noticias/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Noticia, Tag, Comentario, Bairro
+from ..models import Noticia, Tag, Comentario, Bairro, Perfil, Visualizacao, Categoria, Curtida, Usuario
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,10 +27,63 @@ class BairroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bairro
         fields = ['id', 'bairro', 'latitude', 'longitude']
- 
+
+class PerfilSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Perfil
+        fields = ['id', 'perfil']
+        
+class UsuarioSimplesSerializer(serializers.ModelSerializer):
+    perfil = PerfilSerializer(read_only=True)
+
+    class Meta:
+        model = Usuario
+        fields = ['email_usuario', 'nome_usuario', 'perfil']
+
+class AtualizarUsuarioSerializer(serializers.ModelSerializer):
+    senha = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = Usuario
+        fields = ['nome_usuario', 'senha']
+
+    def update(self, instance, validated_data):
+        # Atualiza nome
+        instance.nome_usuario = validated_data.get('nome_usuario', instance.nome_usuario)
+        
+        # Atualiza senha
+        senha = validated_data.get('senha', None)
+        if senha:
+            instance.set_password(senha)
+
+        instance.save()
+        return instance
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ["id", "categoria"]
+        
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ["id", "name", "slug"]
+
+class VisualizacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Visualizacao
+        fields = ['id', 'noticia', 'quantidade']
+
+class CurtidaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curtida
+        fields = ['id', 'usuario', 'noticia', 'created_at']
+        read_only_fields = ['usuario', 'created_at']
+
 class ComentarioSerializer(serializers.ModelSerializer):
-    usuario = serializers.StringRelatedField()
-    
+    usuario = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Comentario
-        fields = ['id', 'usuario', 'conteudo', 'created_at']
+        fields = ['id', 'usuario', 'noticia', 'comentario', 'created_at']
+        read_only_fields = ['id', 'usuario', 'created_at']
--- noticias/api_views.py
+++ noticias/api_views.py
@@ -128,7 +128,185 @@ def listar_bairros(request):
     return Response(serializer.data)
 
 # ----------------------------------------------------------------------  
+# GET Perfil
+@api_view(['GET'])
+def listar_perfis(request):
+    perfis = Perfil.objects.all().order_by('perfil')
+    serializer = PerfilSerializer(perfis, many=True)
+    return Response(serializer.data)
+
+# ----------------------------------------------------------------------  
+# GET Temas
+@api_view(['GET'])
+def listar_temas(request):
+    temas = Tag.objects.all().order_by('name')
+    serializer = TagSerializer(temas, many=True)
+    return Response(serializer.data)
+
+# ----------------------------------------------------------------------  
+# GET Categorias (Real ou Fake)
+
+@api_view(['GET'])
+def listar_categorias(request):
+    categorias = Categoria.objects.all().order_by('categoria')
+    serializer = CategoriaSerializer(categorias, many=True)
+    return Response(serializer.data)
+
+# ----------------------------------------------------------------------  
+# CRUD Visualizações
+@api_view(['GET'])
+def listar_visualizacoes(request):
+    visualizacoes = Visualizacao.objects.all().order_by('id')
+    serializer = VisualizacaoSerializer(visualizacoes, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def criar_visualizacao(request):
+    """
+    Recebe: { "noticia": <id_da_noticia> }
+    """
+    data = request.data.copy()
+
+    # quantidade padrão = 0 se não enviada
+    if "quantidade" not in data:
+        data["quantidade"] = 0
+
+    serializer = VisualizacaoSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# PUT — Atualiza visualização por ID
+@api_view(['PUT'])
+def atualizar_visualizacao(request, id):
+    """
+    Recebe o ID na tabela visualização para atualizar
+    """
+    try:
+        visualizacao = Visualizacao.objects.get(id=id)
+    except Visualizacao.DoesNotExist:
+        return Response({"erro": "Visualização não encontrada"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = VisualizacaoSerializer(visualizacao, data=request.data, partial=True)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# ----------------------------------------------------------------------  
+# CRUD Curtidas
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def listar_curtidas(request):
+    curtidas = Curtida.objects.all().order_by('-created_at')
+    serializer = CurtidaSerializer(curtidas, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def criar_curtida(request):
+    """
+    Recebe o id da notícia e usuário precisa estar logado
+    {
+    "noticia": 12
+    }
+    """
+    noticia_id = request.data.get('noticia')
+
+    if not noticia_id:
+        return Response({"erro": "Campo 'noticia' é obrigatório."}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Verifica se o usuário já curtiu a notícia
+    if Curtida.objects.filter(usuario=request.user, noticia_id=noticia_id).exists():
+        return Response({"erro": "Você já curtiu esta notícia."}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Cria a curtida
+    curtida = Curtida(usuario=request.user, noticia_id=noticia_id)
+    curtida.save()
+
+    serializer = CurtidaSerializer(curtida)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deletar_curtida(request, id):
+    try:
+        curtida = Curtida.objects.get(id=id, usuario=request.user)
+    except Curtida.DoesNotExist:
+        return Response({"erro": "Curtida não encontrada."}, status=status.HTTP_404_NOT_FOUND)
+
+    curtida.delete()
+    return Response({"mensagem": "Curtida removida com sucesso."}, status=status.HTTP_204_NO_CONTENT)
+
+# ----------------------------------------------------------------------  
+# CRUD Comentários
+# GET — lista todos os comentários
+@api_view(['GET'])
+def listar_comentarios(request):
+    comentarios = Comentario.objects.all().order_by('-created_at')
+    serializer = ComentarioSerializer(comentarios, many=True)
+    return Response(serializer.data)
+
+# POST — cria comentário (usuário logado)
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def criar_comentario(request):
+    """
+    Exemplo de POST
+    {
+    "noticia": 12,
+    "comentario": "Ótima notícia!"
+    }
+    """
+    serializer = ComentarioSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(usuario=request.user)  # passa o usuário logado diretamente
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# PUT — atualiza comentário (somente se for do usuário logado)
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def atualizar_comentario(request, id):
+    try:
+        comentario = Comentario.objects.get(id=id, usuario=request.user)
+    except Comentario.DoesNotExist:
+        return Response({"erro": "Comentário não encontrado ou você não tem permissão."}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ComentarioSerializer(comentario, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# DELETE — remove comentário (somente do usuário logado)
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deletar_comentario(request, id):
+    try:
+        comentario = Comentario.objects.get(id=id, usuario=request.user)
+    except Comentario.DoesNotExist:
+        return Response({"erro": "Comentário não encontrado ou você não tem permissão."}, status=status.HTTP_404_NOT_FOUND)
+
+    comentario.delete()
+    return Response({"mensagem": "Comentário removido com sucesso."}, status=status.HTTP_204_NO_CONTENT)
+
+# ----------------------------------------------------------------------  
 # CRUD Login
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def usuario_logado(request):
+    usuario = request.user  # pega o usuário autenticado
+    serializer = UsuarioSimplesSerializer(usuario)
+    return Response(serializer.data)
+
 @api_view(['POST'])
 def cadastro_api(request):
     """
@@ -190,3 +368,13 @@ def login_api(request):
             "mensagem": "Erro no login.",
             "erros": form.errors
         }, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def atualizar_usuario(request):
+    usuario = request.user
+    serializer = AtualizarUsuarioSerializer(usuario, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"mensagem": "Usuário atualizado com sucesso."})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
