83c6b02c275d36a4b63cd57faf57f2fa63f8aee0
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2025-11-27 17:36:28 -0300

    merge

--- portal/settings.py
+++ portal/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^bslunvia86+-ouu3al8$3az$92ty(@pj=u=5%b@cf$c4-i)mp" ### MODIFICADO ###
+SECRET_KEY = os.getenv("SECRET_KEY") ### MODIFICADO ###
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True' ### MODIFICADO ###
