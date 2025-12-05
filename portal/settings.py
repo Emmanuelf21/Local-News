15a17c201f63ce92eeb6406e81e79c0cb8e7846f
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2025-12-05 16:37:57 -0300

    settings

--- portal/settings.py
+++ portal/settings.py
@@ -167,7 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 STATIC_URL = '/static/'
 
-STATIC_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"
